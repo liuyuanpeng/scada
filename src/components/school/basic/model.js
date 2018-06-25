a1859a29e3942e29c276b42e9e395db430c2be5b
chenxun <chenxun1990@126.com>
2018-06-25 12:00:50 +0800

    完善学校基本信息请求接口

--- src/components/school/basic/model.js
+++ src/components/school/basic/model.js
@@ -2,9 +2,83 @@ import ce, { request } from 'cat-eye'
 const { api } = request
 
 ce.model({
-  name: '',
+  name: 'college',
   state: {
+  		data: []
   },
   reducers: {},
-  effects: {}
+  effects: {
+  		getList() {
+  			// 获取学校基本信息列表
+  			return api
+  				.get('/college/list', {
+  					complete: () => {
+	          this.setField({ loading: false })
+	        }
+  				})
+  				.then(res => {
+  					this.setField({
+	          data: res ? res.data : []
+	        })
+  				})
+  		},
+	  	getByCollegeId(collegeId) {
+	  		// 获取学校基本信息 by college id
+	  	  return api
+	      .get('/college/id/' + collegeId, {
+	        complete: () => {
+	          this.setField({ loading: false })
+	        }
+	      })
+	      .then(res => {
+	        this.setField({
+	          data: res ? res.data : []
+	        })
+	      })
+	  	},
+	  	getByCollegeCode(collegeCode) {
+	  		// 获取学校基本信息 by college code
+	  	  return api
+	      .get('/college/code/' + collegeCode, {
+	        complete: () => {
+	          this.setField({ loading: false })
+	        }
+	      })
+	      .then(res => {
+	        this.setField({
+	          data: res ? res.data : []
+	        })
+	      })
+	  	},
+	  	addCollege(college) {
+	  		// 创建学校基本信息
+	  		return api
+	  			.post('/college/create', {
+	  				data: college,
+	  				complete: () => {
+	          this.setField({ loading: false })
+	        }
+	  			})
+	  			.then(res => {
+	  				this.setField({
+	          data: res ? res.data : []
+	        })
+	  			})
+	  	},
+	  	saveCollege(college) {
+	  		// 更新学校基本信息
+	  	  return api
+		  	  	.put('/college/save', {
+		  	  		data: college,
+		  	  		complete: () => {
+	          this.setField({ loading: false })
+	        }
+		  	  	})
+		  	  	.then(res => {
+		  	  		this.setField({
+	          data: res ? res.data : []
+	        })
+		  	  	})
+	  	}
+  }
 })
