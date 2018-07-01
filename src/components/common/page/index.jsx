70846a097c962aa7c1b37653ff36a703ef80e2d0
liuyuanpeng <liuyuanpeng@outlook.com>
2018-07-01 20:09:55 +0800

    导出，下载

--- src/components/common/page/index.jsx
+++ src/components/common/page/index.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Upload from '../upload'
 import { smart, actions } from 'cat-eye'
-import { Select } from 'antd'
+import { Select, Button, Icon } from 'antd'
+import styles from './style'
 
 const Option = Select.Option
 
@@ -16,6 +17,7 @@ export default smart(state => {
 })(class extends Component {
     static propTypes = {
       importUri: PropTypes.string, // 导入服务的api
+      downloadUri: PropTypes.string, // 下载uri
       showYear: PropTypes.bool, // 显示年份选择
       configType: PropTypes.string // 类型组名
     }
@@ -32,6 +34,10 @@ export default smart(state => {
 
     onTypeChange = value => {
       actions.page.setType(value)
+    }
+
+    getExport = () => {
+      window.open(`http://119.23.9.49/v0.1/ceqas/${this.props.downloadUri}/export?is-template=false&token=${localStorage.getItem('accessToken')}`)
     }
 
     render() {
@@ -53,8 +59,20 @@ export default smart(state => {
               }
             </Select>
           }
+          {this.props.downloadUri &&
+          <div className={styles.float_btn}>
+            <a href={`http://119.23.9.49/v0.1/ceqas/${this.props.downloadUri}/export?is-template=true&token=${localStorage.getItem('accessToken')}`} target="_blank">
+                下载Excel模板
+            </a>
+          </div>}
+          {this.props.downloadUri &&
+          <div className={styles.float_btn}>
+            <Button onClick={this.getExport}>
+              <Icon type="download" />导出
+            </Button>
+          </div>}
           {this.props.importUri &&
-          <div style={{marginBottom: '24px', float: 'right'}}>
+          <div className={styles.float_btn}>
             <Upload
               action={this.props.importUri}
               onSuccess={this.props.onSuccess}
